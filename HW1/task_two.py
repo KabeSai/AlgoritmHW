37430b6222827ffadbabc615c3c2756742d40da2
KabeSai <katnikita@vk.com>
2022-11-17 12:05:41 +0300

    task 2

---
+++ HW1/task_two.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfMatches(n: int) -> int:
+        """ Сложность O(1).
+
+        Args:
+            n(int): the number of teams.
+        Returns:
+            int: number of matches played.
+        """
+        return n-1
